44f10a71d25594f8b51097d29fe040b948481812
Thimmmy <tobilobaojewale@yahoo.com>
2022-10-18 04:43:07 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,3 @@
-#!/bin/bash
-/*
- * File: 1-alphabet.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
-
-/**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
- */
 void print_alphabet(void)
 {
 	char letter;
